b1d0871808fe75719305025edc6ba932bdbab712
Yuri Numerov <achi@hush.ai>
2014-07-26 02:13:02 +0200

    display working

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class ship:
     ~~~
     ~~~
 
-    status variable
     """
 
     self.size=size
@@ -53,8 +52,8 @@ class ship:
     for part in self.coords:
       if part[0]==targetx and part[1]==targety: 
         part[2]="X"
-        return 1
-    return 0
+        return 1,[targetx,targety]
+    return 0,[-1,-1]
 
 class arena:
   """
@@ -69,7 +68,6 @@ class arena:
     """
 
     if size<10: size=10
-
     self.arenarray=[]
     for i in range(size):
       self.arenarray.append([])
@@ -86,6 +84,10 @@ class player:
     self.money=10
     self.ships=[]
 
+  def receivehit(self,x,y):
+    for ship in self.ships:
+      if ship.launch(x,y):
+
 def game(humanplayer):
   """
   Main loop etc etc
@@ -95,12 +97,26 @@ def game(humanplayer):
   varena=arena(-1)
   #Generate """"AI"""" player
   AIplayer=player(random.choice(["Hiei","Musashi","Iku","Hachi"]))
-  totalships={"fishing boat (2)":3,"Bigger fishing boat (3)":2,"battleboat (4)":1,"Carrier (5)":1}
+  totalships={"fishing boat (2)":3,"bigger fishing boat (3)":2,"battleboat (4)":1,"carrier (5)":1}
+  turnmsg=""
+  numbers=[1,2,3,4,5,6,7,8,9,0]
 
   #Ship placement!
   os.system('clear')
-  # for key,value in totalships:
-  #   print 
+  for type in totalships:
+    for i in range(totalships[type]):
+      while 1:
+        os.system('clear')
+        print gamename
+        print "%s VS %s" %(humanplayer.name,AIplayer.name)
+        print "  1234567890"
+        for i,j in zip(varena.arenarray,numbers): print str(j)+" "+''.join(map(str,i))
+
+        print "\nPlace your ships with (xcoord,ycoord,vert)"
+        print "Now place your %s"%type
+        placevar=raw_input(">>>")
+        placevar=placevar.split(',')
+
 
 
   while 1:
@@ -109,12 +125,24 @@ def game(humanplayer):
     print gamename
     print "%s VS %s" %(humanplayer.name,AIplayer.name)
     print "  1234567890"
-    for i in varena.arenarray: print str(varena.arenarray.index(i)+1)+" "+''.join(map(str,i))
-    print "\npress F to surrender"
+    for i,j in zip(varena.arenarray,numbers): print str(j)+" "+''.join(map(str,i))
+
+    print "\n%s"%turnmsg
+    print "\ntype the building, action and/or coordinates"
+    print "e.g., 'oil,6,3' or 'bomb,5,0'"
+    print "type F to surrender"
 
     #wait for action
-    loopvar=getch()
+    loopvar=raw_input(">>>")
     if loopvar=="f": break
+    else:
+      loopvar=loopvar.split(',')
+      if loopvar[0]=="oil":
+        turnmsg="You built an oil rig in (%s,%s)"%(loopvar[1],loopvar[2])
+      elif loopvar[0]=="bomb":
+        turnmsg="You dropped a bomb at (%s,%s) \n%s"%(loopvar[1],loopvar[2],"more info")
+      else: turnmsg="Not valid, try again"
+
 
 def getch():
   """
@@ -142,7 +170,9 @@ if __name__=="__main__":
     print "0.- exit"
     mmenu=getch()
     if mmenu=="1":
-      dude=player(raw_input("Player name?> "))
+      playername=raw_input("Player name?> ")
+      if len(playername)<1: playername="Anon"
+      dude=player(playername)
       game(dude)
     elif mmenu=="2":
       os.system('clear')
